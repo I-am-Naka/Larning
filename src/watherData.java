84baa559feb7ccf2004bd40e9f729f64ac6b3546
I-am-Naka <shohei.nakayama7777@gmail.com>
2023-05-21 17:55:45 +0900

    obseverパターン　余分なclass削除+整形

--- src/watherData.java
+++ src/watherData.java
@@ -1,15 +1,13 @@
 
-public class watherData {
-	public class WeatherData {
-		//インスタンス変数宣言
-		public void measurementsChanged() {
-			float temp = getTemperature();
-			float humidity = getHumidity();
-			float pressure = getPressure();
-			
-			currentConditionsDisplay.update(temp, humidity, pressure);
-			statisticsDisplay.update(temp, humidity, pressure);
-			forecastDisplay,update(temp, humidity, pressure);
-		}
+public class WeatherData {
+	//インスタンス変数宣言
+	public void measurementsChanged() {
+		float temp = getTemperature();
+		float humidity = getHumidity();
+		float pressure = getPressure();
+		
+		currentConditionsDisplay.update(temp, humidity, pressure);
+		statisticsDisplay.update(temp, humidity, pressure);
+		forecastDisplay,update(temp, humidity, pressure);
 	}
 }
