10abea12b548de40c5a989266905405de1fe5fe4
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-28 17:03:30 +0900

    designed

--- src/Decorator/CondimentDecorator.java
+++
@@ -1,7 +0,0 @@
-package Decorator;
-
-public abstract class CondimentDecorator extends Beverage {
-	Beverage beverage;
-
-	public abstract String getDescription();
-}
--- src/projava/Calc.java
+++
@@ -1,7 +0,0 @@
-package projava;
-
-public class Calc {
-	public int add(int a, int b) {
-		return a * b;
-	}
-}
