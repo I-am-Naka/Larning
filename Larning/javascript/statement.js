b1a91bb11dbba53b8836a72c15db8c25fc448589
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-29 22:08:10 +0900

    appleSauce()関数の抽出

--- Larning/javascript/statement.js
+++ Larning/javascript/statement.js
@@ -32,15 +32,12 @@ console.log(statement(invoice, plays));
 
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // 注文の内約を出力
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
-    }
+    let totalAmount = appleSauce();
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
@@ -92,4 +89,12 @@ function totalVolumeCredits() {
         volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
+}
+
+function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }
+    return totalAmount;
 }
