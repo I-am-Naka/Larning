f4f2b45d08b429ac5ef077eff16bbe55333397c9
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-29 23:27:10 +0900

    前半フェーズのコードを独立した関数として抽出

--- Larning/javascript/statement.js
+++ Larning/javascript/statement.js
@@ -28,14 +28,18 @@ console.log(statement(invoice, plays));
 
 
 
+function statement (invoice, plays){
+    return renderPlainText(createStatementData(invoice, plays));
+}
 
-function statement(invoice, plays) {
+
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
