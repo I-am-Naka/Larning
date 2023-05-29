e908d5b1195e0e3a5ab2635756e9745e19d69998
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-29 23:03:24 +0900

    amountFor移行

--- Larning/javascript/statement.js
+++ Larning/javascript/statement.js
@@ -38,23 +38,13 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-}
-
-function renderPlainText(data, plays) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        // 注文の内約を出力
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
 
     function amountFor(aPerformance) {
         let result = 0;
@@ -77,6 +67,18 @@ function renderPlainText(data, plays) {
         }
         return result;
     }
+}
+
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        // 注文の内約を出力
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
+
 
 
     function volumeCreditsFor(aPerformance) {
@@ -104,7 +106,7 @@ function renderPlainText(data, plays) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
