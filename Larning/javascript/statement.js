c0cd40f8f8a989aef60a9afa6b8683255a9b0a99
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-29 22:59:38 +0900

    playForをstatementへ移行

--- Larning/javascript/statement.js
+++ Larning/javascript/statement.js
@@ -22,9 +22,9 @@ const playJson = {
     "othello": { "name": "Othello", "type": "tragedy" }
 };
 
-const data = JSON.parse(JSON.stringify(invoiceJson));
+const invoice = JSON.parse(JSON.stringify(invoiceJson));
 const plays = JSON.parse(JSON.stringify(playJson));
-console.log(statement(data, plays));
+console.log(statement(invoice, plays));
 
 
 
@@ -37,7 +37,12 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
         return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
     }
 }
 
@@ -45,7 +50,7 @@ function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         // 注文の内約を出力
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -53,7 +58,7 @@ function renderPlainText(data, plays) {
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -68,19 +73,16 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
         return result;
     }
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
 
-    function volumeCreditsFor(perf) {
+    function volumeCreditsFor(aPerformance) {
         let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
         return volumeCredits;
     }
 
