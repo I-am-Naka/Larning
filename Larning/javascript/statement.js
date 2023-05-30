df429902a8be42f6a728f2eba9f3e41eb8d06675
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-05-30 23:49:23 +0900

    split the file

--- Larning/javascript/statement.js
+++ Larning/javascript/statement.js
@@ -1,3 +1,5 @@
+//import createStatementData from './createStatementData';
+
 const invoiceJson = {
     "customer": "BigCo",
     "performances": [
@@ -29,15 +31,26 @@ console.log(htmlStatement(invoice, plays));
 
 
 
-function statement (invoice, plays){
+function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
-function htmlStatement (invoice, plays) {
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        // 注文の内約を出力
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
     return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderHtml (data) {
+function renderHtml(data) {
     let result = `<h1>Statement for ${data.customer}</h1>\n`;
     result += "<table>\n";
     result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
@@ -51,75 +64,6 @@ function renderHtml (data) {
     return result;
 }
 
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
-        return volumeCredits;
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
-}
-
-function renderPlainText(data, plays) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        // 注文の内約を出力
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(data.totalAmount)}\n`;
-    result += `You earned ${data.totalVolumeCredits} credits\n`;
-    return result;
-
-}
 
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
