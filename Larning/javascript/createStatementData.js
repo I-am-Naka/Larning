32bdfc1c2caff7b807558b317d7f0f5593892c17
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-06-01 00:39:35 +0900

    オーバーライド: get amount()

--- Larning/javascript/createStatementData.js
+++ Larning/javascript/createStatementData.js
@@ -5,34 +5,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy":
-                // オーバーライド
-                // result = 40000;
-                // if (this.performance.audience > 30) {
-                //     result += 1000 * (this.performance.audience - 30);
-                // }
-                // break;
-                throw '想定外の呼び出し' // 念の為throw
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
-    }
-
-    get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        throw new Error('サブクラスの責務');
     }
 }
 
@@ -89,5 +62,12 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
-
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
