380fa949b653d0575911c779a2267c9fa446e286
I-am-Naka <shohei.nakyama7777@gmail.com>
2023-06-01 00:26:49 +0900

    サブクラスによるタイプコードの置き換え: class Tragedy Comedy

--- Larning/javascript/createStatementData.js
+++ Larning/javascript/createStatementData.js
@@ -69,5 +69,17 @@ function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+        default: throw new Error(`知の演劇の種類: ${aPlay.type}`);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+
 }
